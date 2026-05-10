08ce87ecdbd331a177e40839cee3187489b81c8a
vanhouttestien <stien@vanhoutte.eu.com>
2026-05-10 22:02:15 +0200

    fitx for netifly

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 
-const base = "/moose-lotta-adventures/";
-
-// ✅ restore deep link from GitHub Pages 404 redirect
-const redirect = sessionStorage.redirect;
-
-if (redirect) {
-  sessionStorage.removeItem("redirect");
-
-  window.history.replaceState(
-    null,
-    "",
-    base + redirect.replace(/^\/+/, "")
-  );
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
